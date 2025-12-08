5cca57d9cd19ad57cbe774ba9aacc9ecee4a40bb
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-12-08 11:39:31 +0300

    Remove redundant message types

--- src/main/java/common/MessageType.java
+++ src/main/java/common/MessageType.java
@@ -4,6 +4,4 @@ public enum MessageType {
     INPUT,      // Command sent from Client to Server
     OUTPUT,     // Standard output from Server to Client
     ERROR,      // Error output from Server to Client
-    CONNECT,    // Connection initiation
-    DISCONNECT  // Connection termination
 }
