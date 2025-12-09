1b243ee359d47ee054cffb993d910413709241df
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-12-09 08:56:05 +0300

    Add CLI PORT input handling

--- src/main/java/server/ServerMain.java
+++ src/main/java/server/ServerMain.java
@@ -3,11 +3,53 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerMain {
-    private static final int PORT = 8189;
+    private static int PORT = 8189;
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(
+                "Select desired option:\n" +
+                        "1. Use default PORT 8189\n" +
+                        "2. Input your own PORT"
+        );
+        int answer = 0;
+        while (true) {
+            String line = input.nextLine();
+            try {
+                answer = Integer.parseInt(line.trim());
+                if (answer == 1 || answer == 2) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Try again:");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Try again:");
+            }
+        }
+        switch (answer) {
+            case 1:
+                break;
+            case 2:
+                while (true) {
+                    try {
+                        System.out.print("Input desired PORT (value between 1 and 65535): ");
+                        String line = input.nextLine();
+                        int portCandidate = Integer.parseInt(line.trim());
+                        if (portCandidate < 1 || portCandidate > 65535) {
+                            System.out.println("Port number must be between 1 and 65535");
+                            continue;
+                        }
+                        PORT = portCandidate;
+                        break;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input");
+                    }
+                }
+                break;
+        }
         System.out.println("Server started on port " + PORT + "...");
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (true) {
